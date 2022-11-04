6425f29a3eac2e37817e28bc61fca8afe399d690
msrblack <msr.blackx@gmail.com>
2022-11-04 21:20:01 +0300

    Fire

---
+++ homework4/src/Main.java
@@ -0,0 +1,30 @@
+//public class Main {
+//    public static int totalGrade(int num1, int num2){
+//        int num3 = num1 + num2;
+//        return num3;
+//
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(totalGrade(50, 50));
+//
+//    }
+//    public static int temperature(int num1){
+//
+//        if( num1 >= 23 ){
+//            System.out.println("Hot");
+//        }
+//        else {
+//            System.out.println("cold");
+//        }
+//        return num1;
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(temperature(23));
+//    }
+//    public static void main(String[] args) {
+//        for(int x = 1; x <= 10; x++){
+//            System.out.println(x);
+//        }
+//    }
+//
+//}
